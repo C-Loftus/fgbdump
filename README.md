358073de56a499613144bef7bde4f1d5f06d7b73
Colton Loftus <70598503+C-Loftus@users.noreply.github.com>
2025-12-29 17:58:43 -0500

    add demo file

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # fgbdump
+
+fgbdump prints the contents of a [FlatGeobuf](https://flatgeobuf.org/) file in a human-readable terminal user interface.
+
+## Demo
+
+![fgbdump demo](https://github.com/C-Loftus/fgbdump/raw/main/demo.gif)
+
+## Installation
+
+```sh
+cargo install --git https://github.com/c-loftus/fgbdump
+```
--- src/cli.rs
+++ src/cli.rs
@@ -1,13 +1,16 @@
+// Copyright 2025 Colton Loftus
+// SPDX-License-Identifier: Apache-2.0
+
 use argh::FromArgs;
 
 #[derive(FromArgs, Debug)]
-/// Display info about a FlatGeobuf file
+/// Print info about a FlatGeobuf file. Author: Colton Loftus
 pub struct Args {
     #[argh(positional)]
     /// the FlatGeobuf file to inspect
     pub file: String,
 
     #[argh(switch)]
-    /// output flatgeobuf info to stdout
+    /// output flatgeobuf info to stdout instead of the TUI
     pub stdout: bool,
 }
